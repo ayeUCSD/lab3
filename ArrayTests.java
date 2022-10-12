428c8ff4160a22ddf75f95e1497c1f9d73228dcd
ayeUCSD <114363008+ayeUCSD@users.noreply.github.com>
2022-10-12 16:55:52 -0700

    fixed 2/3 in ArrayExamples

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,22 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+
+  @Test
+  public void testReverseInPlaceLen5(){
+    int[] input = {1,2,3,4,5};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input);
+  }
+
+
+  @Test
+  public void testReversedLen5(){
+    int[] input = {1,2,3,4,5};
+    int[] output = ArrayExamples.reversed(input);
+    assertArrayEquals(new int[]{5,4,3,2,1}, output);
+  }
+
+
 }
